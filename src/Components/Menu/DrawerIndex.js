c8438af47628acdb2e3d861177a37c15caa2a5b1
Carvalh0R0b3rt <carvalhrobert561@gmail.com>
2025-07-25 19:28:49 -0300

    Adicionando o menu dentro do drawer

--- src/Components/Menu/DrawerIndex.js
+++ src/Components/Menu/DrawerIndex.js
@@ -1,8 +1,93 @@
 import { useState } from "react";
 import { Button, Drawer, Space } from "antd";
 import "./Drawer.css";
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
 
-const DrawerIndex = () => {
+const items = [
+  {
+    key: 'sub1',
+  label: 'Navigation One',
+  icon: <MailOutlined />,
+  children: [
+  {
+    key: 'g1',
+  label: 'Item 1',
+  type: 'group',
+  children: [
+  {key: '1', label: 'Option 1' },
+  {key: '2', label: 'Option 2' },
+  ],
+  },
+  {
+    key: 'g2',
+  label: 'Item 2',
+  type: 'group',
+  children: [
+  {key: '3', label: 'Option 3' },
+  {key: '4', label: 'Option 4' },
+  ],
+},
+  ],
+},
+  {
+    key: 'sub2',
+  label: 'Navigation Two',
+  icon: <AppstoreOutlined />,
+  children: [
+  {key: '5', label: 'Option 5' },
+  {key: '6', label: 'Option 6' },
+  {
+    key: 'sub3',
+  label: 'Submenu',
+  children: [
+  {key: '7', label: 'Option 7' },
+  {key: '8', label: 'Option 8' },
+  ],
+},
+  ],
+},
+  {
+    type: 'divider',
+},
+  {
+    key: 'sub4',
+  label: 'Navigation Three',
+  icon: <SettingOutlined />,
+  children: [
+  {key: '9', label: 'Option 9' },
+  {key: '10', label: 'Option 10' },
+  {key: '11', label: 'Option 11' },
+  {key: '12', label: 'Option 12' },
+  ],
+},
+  {
+    key: 'grp',
+  label: 'Group',
+  type: 'group',
+  children: [
+  {key: '13', label: 'Option 13' },
+  {key: '14', label: 'Option 14' },
+  ],
+},
+  ];
+const MenuIndex = () => {
+const onClick = e => {
+    console.log('click ', e);
+};
+  return (
+  <Menu
+    onClick={onClick}
+    style={{ width: 256 }}
+    defaultSelectedKeys={['1']}
+    mode="inline"
+    items={items}
+  />
+  );
+};
+
+
+function DrawerIndex() {
   const [open, setOpen] = useState(false);
   const [placement] = useState("left");
   const showDrawer = () => {
@@ -19,13 +104,6 @@ const DrawerIndex = () => {
   return (
     <>
       <Space>
-        {/*<Radio.Group value={placement} onChange={onChange}>
-          <Radio value="top">top</Radio>
-          <Radio value="right">right</Radio>
-          <Radio value="bottom">bottom</Radio>
-          <Radio value="left">left</Radio>
-        </Radio.Group>
-        */}
         <Button className="Primary" type="primary" onClick={showDrawer}>
           ☲ Menu
         </Button>
@@ -39,11 +117,10 @@ const DrawerIndex = () => {
         open={open}
         key={placement}
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <MenuIndex/>
+
       </Drawer>
     </>
   );
-};
+}
 export default DrawerIndex;
